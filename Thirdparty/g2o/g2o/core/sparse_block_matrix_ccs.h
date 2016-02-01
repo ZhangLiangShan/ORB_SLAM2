0365edc33228c1e36457951c7a98ac2ed82e47b8
yueying <fengbing123@gmail.com>
2016-02-01 17:48:50 +0800

    Update sparse_block_matrix_ccs.h
    restore this file

--- Thirdparty/g2o/g2o/core/sparse_block_matrix_ccs.h
+++ Thirdparty/g2o/g2o/core/sparse_block_matrix_ccs.h
@@ -35,7 +35,11 @@
 #include "matrix_operations.h"
 
 
+#ifdef _MSC_VER
 #include <unordered_map>
+#else
+#include <tr1/unordered_map>
+#endif
 
 
 namespace g2o {
@@ -221,7 +225,7 @@ namespace g2o {
       //! rows of the matrix
       int rows() const {return _rowBlockIndices.size() ? _rowBlockIndices.back() : 0;}
 
-      typedef std::unordered_map<int, MatrixType*> SparseColumn;
+      typedef std::tr1::unordered_map<int, MatrixType*> SparseColumn;
 
       SparseBlockMatrixHashMap(const std::vector<int>& rowIndices, const std::vector<int>& colIndices) :
         _rowBlockIndices(rowIndices), _colBlockIndices(colIndices)
